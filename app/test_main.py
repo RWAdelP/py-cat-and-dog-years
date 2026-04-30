f2f80828c7861a054ee49f877a7a2ee27608f746
Adel Pereira <nic.gis.office@gmail.com>
2026-04-30 08:42:01 +0100

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -2,6 +2,7 @@ import pytest
 
 from app.main import get_human_age
 
+
 @pytest.mark.parametrize(
     "cat_age,dog_age,result",
     [
@@ -19,24 +20,12 @@ from app.main import get_human_age
         (100, 100, [21, 17])
     ]
 )
-def test_all_boundary_ages(cat_age: int, dog_age: int, result: list[int]) -> None:
+def test_all_boundary_ages(
+    cat_age: int,
+    dog_age: int,
+    result: list[int]
+) -> None:
     assert (
         get_human_age(cat_age, dog_age) == result
-        ), f"{cat_age} cat years, should be {result[0]} human years, {dog_age} dog years should be {result[1]} human years."
-    
-"""
-def test_zero_ages():
-    assert get_human_age(0, 0) == [0, 0]
-def test_first_boundary_cat():
-    # 15 cat years = 1 human year
-    assert get_human_age(15, 0) == [1, 0]
-def test_first_boundary_dog():
-    # 15 dog years = 1 human year
-    assert get_human_age(0, 15) == [0, 1]
-def test_second_boundary_cat():
-    # 15 + 9 = 24 cat years = 2 human years
-    assert get_human_age(24, 0) == [2, 0]
-def test_second_boundary_dog():
-    # 15 + 9 = 24 dog years = 2 human years
-    assert get_human_age(0, 24) == [0, 2]
-"""
+    ), (f"{cat_age} cat years, should be {result[0]} human years, "
+        f"{dog_age} dog years should be {result[1]} human years.")
